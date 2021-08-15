9ba88d35cb98b44db3db405677c1945ef70ca37d
62160325 จรัสพร แซ่ล่อ <87239665+Jaraspon@users.noreply.github.com>
2021-08-15 22:59:15 +0700

    การดึงไฟล์

--- application/views/consent/v_test_read_file.php
+++ application/views/consent/v_test_read_file.php
@@ -1,16 +1,8 @@
 <div class="card-header" id="card_radius">
-    <?php
-    for ($i = 0; $i < count($file_emp); $i++) {
-        // if($file_emp[$i]->fil_emp_id ==)
-        $path = "upload/" . $file_emp[$i]->fil_location;
-        // echo $file_emp[$i]->fil_location;
-        // echo $path;
-        echo readfile($path);
-    }
-    ?>
-    <!-- <input type="button" value="Open file"> -->
-
-    <!-- <iframe src="<?php //echo $path 
-                        ?>" type="application/pdf" style="width:100% height:100%"></iframe> -->
+<div id="area"></div>
+<iframe src="<?=$file_emp?>" frameborder="0" style="
+    width: 100%;
+    height: 85vh;
+"></iframe>
 
 </div>
