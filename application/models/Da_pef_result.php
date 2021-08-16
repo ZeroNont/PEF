8a02bfdfdb6c77213939c2ea9340615895dd31a7
jxcho <72914014+jxcho@users.noreply.github.com>
2021-08-16 22:26:30 +0700

    Result

---
+++ application/models/Da_pef_result.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* Da_pef_result
+* Model Result
+* @input  - 
+* @output - 
+* @author Apinya Phadungkit
+* @Create Date 2564-8-15
+* @Update Date 2564-8-16
+*/
+?>
+
+
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+include_once('pefs_model.php');
+
+class Da_pef_result extends pefs_model
+{
+    
+    /*
+    * Function __construct
+    * @input  -   
+    * @output -
+    * @author Apinya Phadungkit
+    * @Create Date 2564-8-15
+    * @Update Date 2564-8-16
+    */
+    function __construct()
+    {
+        parent::__construct();
+    }//__construct
+    
+}
